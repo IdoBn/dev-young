d17e84ccc2f93bef590aa8257da67c6f9d88a9b6
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-05-02 07:42:39 +0300

    adding routes for sessions controller and facebook auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  match 'auth/:provider/callback', to: 'sessions#create', via: :post
+  get 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout', via: :delete
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

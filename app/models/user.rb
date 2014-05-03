a36718b8b4a7ba46f1fd142b6d47d31f0c507e91
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-05-03 16:38:26 +0300

    adding own method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
 	has_one :group
 
+	def owns?(obj)
+		obj.user == self
+	end
+
 	def self.from_omniauth(auth)
 	  where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
 	    user.provider = auth.provider

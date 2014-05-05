bb8173f3670f33a1c91b59ef32984b603bbeb3b9
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-05-06 01:57:13 +0300

    adding confirmation to request

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,13 +4,20 @@ class Request < ActiveRecord::Base
 
 	validates_uniqueness_of :user_id, :scope => :group_id
 	validate :user_can_request
-	# before_save :user_can_request
+	
+	after_save :check_confirm
 
 	private
 		def user_can_request
 			@user = self.user
 			if @user
 				errors.add :user, 'user cannot own a group' unless @user.can_request?
+			end
+		end
+
+		def check_confirm
+			if self.group_confirm && self.user_confirm
+				self.group.users << self.user
 			end
 		end
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -1,4 +1,19 @@
 require 'spec_helper'
 
 describe Request do
+	it 'adds user to group when group confirms' do
+		expect{
+			request = user2.new_request(group1)
+			request.save
+			request.update_attribute(:group_confirm, true)
+		}.to change{ group1.users.count }.by(1)
+	end
+
+	it 'adds user to group when user confirms' do
+		expect {
+			request = group1.new_request(user2)
+			request.save
+			request.update_attributes(user_confirm: true)
+		}.to change{ group1.users.count }.by(1)
+	end
 end

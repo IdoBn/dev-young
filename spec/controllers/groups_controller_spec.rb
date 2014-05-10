1636618be3a8133e6340bc2873297588292c7005
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-05-10 13:04:36 +0300

    adding more specs to users and groups controller

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,29 +1,44 @@
 require 'spec_helper'
 
 describe GroupsController do
-	before(:each) do
-		group1
-		# ApplicationController.any_instance.stub(:current_user).and_return(user1)
-    controller.stub(:current_user).and_return(user1)
-  end
+	before(:each) {group1}
 
-	context 'GET #index' do
-		before(:each) { get :index, query: 'Maria' }
+	context 'signed in' do
+		before(:each) do
+	    controller.stub(:current_user).and_return(user1)
+	  end
 
-		it { expect(assigns(:groups)).to eq([group1]) }
-		it { expect(response).to render_template(:index) }
-	end
+		context 'GET #index' do
+			before(:each) { get :index, query: 'Maria' }
 
-	[:show, :edit].each do |action|
-		context "GET ##{action}" do
-			before(:each) { get action, id: user1.group.id }
-			
-			describe 'signed in' do
+			it { expect(assigns(:groups)).to eq([group1]) }
+			it { expect(response).to render_template(:index) }
+		end
+
+		[:show, :edit].each do |action|
+			context "GET ##{action}" do
+				before(:each) { get action, id: user1.group.id }
+				
 				it { expect(assigns(:group)).to eq(user1.group) }
 				it { expect(response).to render_template(action) }
 			end
+		end
+	end
 
-			describe 'signed out' do
+	context 'signed out' do
+		context 'GET #index' do
+			before(:each) { get :index, query: 'Maria' }
+
+			it { expect(assigns(:groups)).to_not eq([group1]) }
+			it { expect(response).to_not render_template(:index) }
+		end
+
+		[:show, :edit].each do |action|
+			context "GET ##{action}" do
+				before(:each) { get action, id: user1.group.id }
+				
+				it { expect(assigns(:group)).to_not eq(user1.group) }
+				it { expect(response).to_not render_template(action) }
 			end
 		end
 	end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,21 +1,59 @@
 require 'spec_helper'
 
 describe UsersController do
-  before :each do
-    controller.stub(:current_user).and_return(user1)
-  end
+  context 'Authorized' do
+    before :each do
+      controller.stub(:current_user).and_return(user1)
+    end
 
-  describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+      end
+
+      it 'returns correct query' do
+        get :index, query: 'Maria'
+        expect(assigns(:users)).to eq([user1])
+      end
+    end
+
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show', id: user2.id
+        response.should be_success
+      end
+
+      it 'returns correct user' do
+        get :show, id: user1.id
+        expect(assigns(:user)).to eq(user1)
+      end
     end
   end
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show', id: user2.id
-      response.should be_success
+  context 'nonAuthorized' do
+    describe "GET 'index'" do
+      it "does not returns http success" do
+        get 'index'
+        response.should_not be_success
+      end
+
+      it 'does not returns correct query' do
+        get :index, query: 'Maria'
+        expect(assigns(:users)).to_not eq([user1])
+      end
+    end
+
+    describe "GET 'show'" do
+      it "does not returns http success" do
+        get 'show', id: user2.id
+        response.should_not be_success
+      end
+
+      it 'does not returns correct user' do
+        get :show, id: user1.id
+        expect(assigns(:user)).to_not eq(user1)
+      end
     end
   end
 

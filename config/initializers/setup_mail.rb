666b4c83e3513403e7281ed040d5fbde40dd195f
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-05-11 13:56:41 +0300

    checking mandrill

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,11 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.mandrillapp.com",
-  :port                 => 587,
-  :domain               => "heroku.com",
-  :user_name            => ENV['MANDRILL_USERNAME'],
-  :password             => ENV['MANDRILL_APIKEY'],
-  :authentication       => "plain",
-  :enable_starttls_auto => true
+  :port =>           '587',
+  :address =>        'smtp.mandrillapp.com',
+  :user_name =>      ENV['MANDRILL_USERNAME'],
+  :password =>       ENV['MANDRILL_APIKEY'],
+  :domain =>         'heroku.com',
+  :authentication => :plain
 }
 
 ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.default_url_options[:host] = "localhost:3000" # this will need to change
+# ActionMailer::Base.default_url_options[:host] = "localhost:3000" # this will need to change
